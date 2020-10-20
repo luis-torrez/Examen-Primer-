07c95b9f97986348cc1b122746ab50445224bfcc
Luis Gabriel Torrez Rojas <73142440+luis-torrez@users.noreply.github.com>
2020-10-20 00:12:06 -0400

    Add files via upload

---
+++ cambia.php
@@ -0,0 +1,15 @@
+<?php 
+	$color = $_POST['color'];
+	$ci = $_POST['ci'];
+
+	require 'conexion.inc.php';
+	
+//actualizar datos
+	$actualizar = "update usuario set color='$color' where ci='$ci'";
+	$resultado = mysqli_query($conexion,$actualizar);
+	if ($resultado) {
+		include("home.php");
+	}else
+	{
+		echo "=====";
+	}
